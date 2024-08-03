bf69ca39ca5dccbced46263b3dc1a01bffe015ff
Charles Vincent <cjmvincent@outlook.com>
2024-08-03 07:47:35 -0400

    Add/Remove scripts

---
+++ Powershell/DHCP/Configure-DHCPOptions.ps1
@@ -0,0 +1,5 @@
+﻿$server = "bcboe-fs2"
+$option = "ntp server"
+$optionid = "042"
+
+Set-DhcpServerv4OptionValue -ComputerName $server -OptionId $optionid -Value 10.8.0.120,10.8.0.121
---
+++ Powershell/DHCP/Test-DeviceList.ps1
@@ -0,0 +1,15 @@
+﻿cd C:\temp
+$Devices = ( Import-Csv -Path .\trash.csv )
+$GW = @()
+$Unreachable = @()
+foreach ( $device in $Devices ) {
+    if ( Test-Connection -ComputerName $device.IPAddress -Count 2 ) {
+        $GW += $device
+    } elseif ( !( Test-Connection -ComputerName $device.IPAddress -Count 2) ) {
+        $Unreachable += $device
+    }
+}
+Write-Host "These devices were reachable at:"
+$GW | Format-Table -AutoSize
+Write-Host "These devices were not reachable:"
+$Unreachable | Format-Table -AutoSize
