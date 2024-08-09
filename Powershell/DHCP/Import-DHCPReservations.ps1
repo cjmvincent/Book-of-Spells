588b37dd3ca019cf734dc8acc28ac9f84c561dc8
Charles Vincent <cjmvincent@outlook.com>
2024-08-09 18:18:15 -0400

    switch from csv to excel

--- Powershell/DHCP/Import-DHCPReservations.ps1
+++ Powershell/DHCP/Import-DHCPReservations.ps1
@@ -1,12 +1,13 @@
 # csv should have the following headers: dhcpserver,scopeid,ipaddr,name,mac,description
 # example: 10.10.10.0,10.10.10.10,Computer1,1a-1b-1c-1d-1e-1f,Reserved for Computer
 
-$path = C:\temp
-$file = dhcpreservations.csv
+$path = "C:\temp"
+$file = "dhcpreservations.xlsx"
+$sheet = "Sheet1"
 
-$reservations = Import-Csv -Path "$path\$file"
+$reservations = Import-Excel -Path $path\$file -WorksheetName $sheet
 
 foreach ( $reservation in $reservations ) {
-    Write-Host "Creating reservation for $reservation.name $reservation.mac"
-    Add-DhcpServerv4Reservation -ComputerName $reservation.dhcpserver -ScopeId $reservation.scopeid -IPAddress $reservation.ipaddr -Name "$reservation.name" -ClientId "$reservation.mac" -Description "$reservation.description"
+    Write-Host "Creating reservation for $($reservation.name) $($reservation.mac)"
+    Add-DhcpServerv4Reservation -ComputerName $reservation.dhcpserver -ScopeId $reservation.scopeid -IPAddress $reservation.ipaddr -Name $reservation.name -ClientId $reservation.mac -Description $reservation.description
 }
