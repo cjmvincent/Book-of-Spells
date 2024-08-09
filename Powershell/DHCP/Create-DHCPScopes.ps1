39f6baf6abc6f61f4b848be2f0fb5641c26e879e
Charles Vincent <cjmvincent@outlook.com>
2024-08-09 18:29:01 -0400

    fix var and add template for dhcpscopes

--- Powershell/DHCP/Create-DHCPScopes.ps1
+++ Powershell/DHCP/Create-DHCPScopes.ps1
@@ -1,12 +1,13 @@
 ﻿#csv header format name;dhcpserver;startrange;endrange;subnetmask;scopeid;router
 
-$path = C:\temp
-$file = dhcpreservations.csv
+$path = "C:\temp"
+$file = "dhcpscopes.xlsx" 
+$sheet = "Sheet1"
 
-$dhcpScopes = Import-Csv -Path "$path\$file"
+$dhcpScopes = Import-Excel -Path $path\$file -WorksheetName $Sheet
 
 Foreach ( $scope in $dhcpScopes) {
-    Write-Output "Creating DHCP Scope $scope.name"
-    Add-DhcpServerv4Scope -ComputerName $scope.dhcpserver -StartRange $scope.startrange -EndRange $scope.endrange -SubnetMask $scope.subnetmask -Name $scope.name -State Active
-    Set-DhcpServerv4OptionValue -ComputerName $scope.dhcpserver -ScopeId $scope.scopeid -Router $scope.router
+    Write-Output "Creating DHCP Scope $($scope.name)"
+    Add-DhcpServerv4Scope -ComputerName $scope.DHCPServer -StartRange $scope.StartRange -EndRange $scope.EndRange -SubnetMask $scope.SubnetMask -Name $scope.Name -State Active
+    Set-DhcpServerv4OptionValue -ComputerName $scope.DHCPServer -ScopeId $scope.scopeID -Router $scope.Router
 }
