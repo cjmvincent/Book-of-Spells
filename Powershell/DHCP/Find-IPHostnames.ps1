253dd84a11a07afaa719cafad98167498943f4c9
Charles Vincent <cjmvincent@outlook.com>
2024-11-20 10:19:54 -0500

    finding dhcp hostnames from list of ip address

---
+++ Powershell/DHCP/Find-IPHostnames.ps1
@@ -0,0 +1,38 @@
+Import-Module importexcel
+
+$Source_Path = "C:\temp"
+$Source_File = "dhcp_stuff.xlsx"
+$Source_Worksheet = "Hosts"
+
+$Devices = Import-Excel -Path $Source_Path\$Source_File  -Worksheet $Source_Worksheet
+
+$Dest_Path = "C:\temp"
+$Dest_File = "dhcp_stuff.xlsx"
+$Dest_Worksheet = "Found"
+
+$Global:Data = @()
+Function Find_NamedDevices ($Server){
+
+    $All_IPs = Get-DhcpServerv4Scope -ComputerName $Server | Get-DhcpServerv4Lease -ComputerName $Server
+    ForEach($IP in $All_IPs){
+        ForEach ($Device in $Devices){
+            If ($IP.IPAddress -like "*$($Device.IPAddress)*"){
+                $Device = [PSCustomObject]@{
+                    Hostname = $IP.Hostname;
+                    ClientID = $IP.ClientID;
+                    IPAddress = $IP.IPAddress
+                }
+            $Device | Export-Excel -Path $Dest_Path\$Dest_File -WorksheetName $Dest_Worksheet -Append
+            }
+        }
+    }
+}
+
+$servers = @(
+    "bcboe-fs2"
+    "bcboe-fs3"
+)
+
+foreach ($server in $servers) {
+    Find_NamedDevices -Server $server
+}
