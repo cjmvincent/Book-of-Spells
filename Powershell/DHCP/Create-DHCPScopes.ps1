31fda2cb8b995d84e10be14c0e6b86ed107583bc
Charles Vincent <cjmvincent@outlook.com>
2025-01-15 07:15:54 -0500

    fixed var

--- Powershell/DHCP/Create-DHCPScopes.ps1
+++ Powershell/DHCP/Create-DHCPScopes.ps1
@@ -8,7 +8,7 @@ $sheet = "Scopes"
 $dhcpScopes = Import-Excel -Path $path\$file -WorksheetName $Sheet
 
 Foreach ( $scope in $dhcpScopes) {
-    Write-Output "Creating DHCP Scope $($scope.name)"
+    Write-Output "Creating DHCP Scope $($scope.Name)"
     Add-DhcpServerv4Scope -ComputerName $scope.DHCPServer -StartRange $scope.StartRange -EndRange $scope.EndRange -SubnetMask $scope.SubnetMask -Name $scope.Name -State Active
     Set-DhcpServerv4OptionValue -ComputerName $scope.DHCPServer -ScopeId $scope.ScopeID -Router $scope.Router
 }
