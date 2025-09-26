2b450eb6ba1ca6f5ebb07d6a3577d57ba5c29bd0
Charles Vincent <cjmvincent@outlook.com>
2025-09-26 12:51:04 -0400

    .

--- Powershell/Server Management/Modify-PrintObjects.ps1
+++ Powershell/Server Management/Modify-PrintObjects.ps1
@@ -9,20 +9,20 @@ $printers = Import-Excel -Path $File\$Path -WorksheetName $Sheet
 
 foreach ($printer in $printers) {
 
-    $printer = Get-Printer -ComputerName $printer.Server -Name $printer.PrinterName
-    $port    = Get-PrinterPort -ComputerName $printer.Server -Name $printer.PortName
+    $printerobject = Get-Printer -ComputerName $printer.Server -Name $printer.PrinterName
+    $port    = Get-PrinterPort -ComputerName $printer.Server -Name $printerobject.PortName
 
     # Try updating the existing port
     Set-PrinterPort -ComputerName $printer.Server -Name $port.Name -PrinterHostAddress $printer.NewIPAddress -ErrorAction SilentlyContinue
 
-    # # If it didn’t take (some ports can’t be modified), create new and rebind
-    # if ($port.PrinterHostAddress -ne $printer.NewIPAddress) {
-    #     $newPortName = "IP_$($printer.NewIPAddress)"
-    #     if (-not (Get-PrinterPort -ComputerName $printer.Server -Name $newPortName -ErrorAction SilentlyContinue)) {
-    #         Add-PrinterPort -ComputerName $printer.Server -Name $newPortName -PrinterHostAddress $printer.NewIPAddress
-    #     }
-    #     Set-Printer -ComputerName $printer.Server -Name $printer.PrinterName -PortName $newPortName
-    # }
+    # If it didn’t take (some ports can’t be modified), create new and rebind
+    if ($port.PrinterHostAddress -ne $printer.NewIPAddress) {
+        $newPortName = "IP_$($printer.NewIPAddress)"
+        if (-not (Get-PrinterPort -ComputerName $printer.Server -Name $newPortName -ErrorAction SilentlyContinue)) {
+            Add-PrinterPort -ComputerName $printer.Server -Name $newPortName -PrinterHostAddress $printer.NewIPAddress
+        }
+        Set-Printer -ComputerName $printer.Server -Name $printer.PrinterName -PortName $newPortName
+    }
 
     Write-Host "Updated $($printer.PrinterName) on $($printer.Server) to $($printer.NewIPAddress)"
 }
